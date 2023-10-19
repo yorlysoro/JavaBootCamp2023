c13b5a27c9ac24c91e63882eb6ea23d1345a06c8
yorlysoro <yorlysoro@gmail.com>
2023-10-19 10:36:01 -0400

    Hello World Test

---
+++ src/main/java/com/mycompany/javacourses/JavaCourses.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.javacourses;
+
+/**
+ *
+ * @author yorlys
+ */
+public class JavaCourses {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
